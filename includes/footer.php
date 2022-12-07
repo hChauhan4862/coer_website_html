629dac3deed2922a8251577e309123ec260a5ec3
Harendra Chauhan <charendra71@gmail.com>
2022-12-07 19:21:46 +0530

    to PHP

---
+++ includes/footer.php
@@ -0,0 +1,307 @@
+<footer class="iar-footer">
+	<div class="footer-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-sm-12 col-12 footer-0">
+
+					<div class="footer-logo-wrap">
+						<a href="index.html" class="footer-top-logo">
+							<img src="images/ss.PNG" class="img-fluid" alt="" />
+						</a>
+					</div>
+
+					<div class="widget widget_text">
+						<div class="textwidget">
+							<p>The COER university of engineering, which offers professionally focused undergraduate and
+								postgraduate programs.</p>
+						</div>
+					</div>
+
+					<div class="col-12">
+						<div class="copyright-widget">
+							<div class="widget widget_nav_menu">
+								<div class="menu-copyright-menu-container">
+									<ul id="menu-copyright-menu" class="menu newMenu">
+										<li class="menu-item menu-item-type-post_type"><a href="#">BCA</a></li>
+										<li class="menu-item menu-item-type-post_type"><a href="#">MCA</a></li>
+										<li class="menu-item menu-item-type-post_type"><a href="#">B.Tech</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<ul class="widget footer_social">
+						<li>
+							<a href="#"><i class="fab fa-facebook-f"></i></a>
+						</li>
+						<li>
+							<a href="#/"><i class="fab fa-instagram"></i></a>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-linkedin-in"></i></a>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-twitter"></i></a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="col-lg-3 col-md-6 col-sm-12 col-12 footer-1">
+					<div class="widget widget_contact_widget">
+						<h3 class="footer-title">Get In Touch</h3>
+
+						<ul class="fa-ul touch-ul">
+							<li class="address1">
+								<i class="fas fa-map-marker-alt"></i>
+								<a href="#"><span>7 kms, Roorkee-Haridwar Road (NH-58), Post Box No. 27,Vardhmanpuram,
+										Roorkee-247667 District Haridwar (Uttarakhand)</span></a>
+							</li>
+							<li>
+								<i class="fas fa-mobile-alt"></i>
+								<a href="#">##### #####</a>
+								<a href="#">##### ##### (Admission Enquiries)</a>
+							</li>
+							<li>
+								<i class="fas fa-phone-alt"></i>
+								<a href="#">099 9999 9999 (Reception)</a>
+							</li>
+							<li>
+								<i class="far fa-envelope"></i>
+								<a href="#"><span class="__cf_email__"
+										data-cfemail="40232f2e3421233400232f25326e21236e292e">[email&#160;protected]</span>,</a>
+								<a href="#"><span class="__cf_email__"
+										data-cfemail="c9a8ada4a0babaa0a6a7ba89aaa6acbbe7a8aae7a0a7">[email&#160;protected]</span></a>
+							</li>
+							<li>
+								<i class="far fa-address-card"></i>
+								<a href="#">Apply for Job in COER, kindly contact us on <span class="__cf_email__"
+										data-cfemail="ee84818c9dae8d818b9cc08f8dc08780">[email&#160;protected]</span></a>
+							</li>
+						</ul>
+
+					</div>
+				</div>
+
+				<div class="col-lg-2 col-md-6 col-sm-12 col-12 footer-2">
+					<div class="widget widget_nav_menu">
+						<h3 class="footer-title">important Links</h3>
+						<ul class="menu-courses">
+							<li class="menu-item menu-item-type-custom">
+								<a href="#">Apply for Admission</a>
+							</li>
+							<li class="menu-item menu-item-type-custom">
+								<a href="#">Fee Payment Link</a>
+							</li>
+							<li class="menu-item menu-item-type-post_type">
+								<a href="#">Academic Calendar</a>
+							</li>
+							<li class="menu-item menu-item-type-post_type">
+								<a href="#">Downloads</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 col-sm-12 col-12 footer-3">
+					<div class="recent-widget main-sidebar widget">
+						<h3 class="footer-title">Recent Posts</h3>
+						<div class="recent-post-widget">
+							<div class="show-featured">
+								<div class="post-item">
+									<div class="post-desc">
+										<h5>
+											<a href="#">COER University of Engineering News 1</a>
+										</h5>
+										<ul>
+											<li>
+												<span class="date-post">
+													<i class="far fa-calendar-alt"></i>November 04, 2022</span>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="show-featured">
+								<div class="post-item">
+									<div class="post-desc">
+										<h5>
+											<a href="#">COER News Update 2</a>
+										</h5>
+										<ul>
+											<li>
+												<span class="date-post">
+													<i class="far fa-calendar-alt"></i>November 05, 2022</span>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="footer-bottom">
+		<div class="container">
+			<div class="copyright_border">
+				<div class="rows">
+
+					<div class="col-lg-6 col-md-5 col-sm-12 col-12 frist-cols">
+						<div class="copyright">
+							<p>Copyright © <a href="#" target="_blank">hChauhan4862</a> & <a href="#"
+									target="_blank">tyagi5yashkant89</a>.</p>
+						</div>
+					</div>
+
+					<div class="col-lg-6 col-md-7 col-sm-12 col-12">
+						<div class="copyright-widget">
+							<div class="widget widget_nav_menu">
+								<div class="menu-copyright-menu-container">
+									<ul id="menu-copyright-menu" class="menu">
+										<li class="menu-item-object-page"><a href="#">Privacy Policy</a></li>
+										<li class="menu-item-object-custom"><a href="#">Events</a></li>
+										<li class="menu-item-object-custom"><a href="#">Blog</a></li>
+										<li class="menu-item-object-custom"><a href="#">Contact Us</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+</footer>
+
+
+<div id="scrollUp" class="orange-color">
+	<i class="fa fa-angle-up"></i>
+</div>
+
+<div id="ssb-container" class="ssb-btns-right ssb-anim-slide">
+	<ul class="ssb-dark-hover">
+		<li id="ssb-btn-0">
+			<p>
+				<a href="#" target="_blank">
+					<span class="fas fa-university"></span>Admission
+				</a>
+			</p>
+		</li>
+		<li id="ssb-btn-1">
+			<p>
+				<a href="#" target="_blank">
+					<span class="fas fa-cloud-download-alt"></span>Prospectus
+				</a>
+			</p>
+		</li>
+		<li id="ssb-btn-2">
+			<p>
+				<a href="#" target="_blank">
+					<span class="fas fa-sign-in-alt"></span>Apply Now
+				</a>
+			</p>
+		</li>
+	</ul>
+</div>
+
+<script>
+	var ssb_ui_data = { "z_index": "1" };
+</script>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery-migrate.mind.js"></script>
+<script src="js/ng/controller.js"></script>
+<script src="js/ssb-ui.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/masonry.pkgd.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/magnific-popup.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/jquery.nice-select.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/index.js"></script>
+
+<script data-cfasync="false" src="js/email-decode.min.js"></script>
+
+<script>
+	(function () {
+		function maybePrefixUrlField() {
+			if (this.value.trim() !== '' && this.value.indexOf('http') !== 0) {
+				this.value = "http://" + this.value;
+			}
+		}
+
+		var urlFields = document.querySelectorAll('.mc4wp-form input[type="url"]');
+		if (urlFields) {
+			for (var j = 0; j < urlFields.length; j++) {
+				urlFields[j].addEventListener('blur', maybePrefixUrlField);
+			}
+		}
+	})();
+</script>
+<script src="js/jquery.validate.min.js" id="jquery-validate-js-js"></script>
+<script src="js/script.js" id="script-js-js"></script>
+<script src="js/core.min.js" id="jquery-ui-core-js"></script>
+<script src="js/effect.min.js" id="jquery-effects-core-js"></script>
+<script src="js/effect-shake.min.js" id="jquery-effects-shake-js"></script>
+<script src="js/mind.js" id="mind-js"></script>
+<script src="js/tabs.min.js" id="jquery-ui-tabs-js"></script>
+<script id="cffscripts-js-extra">
+	var cffOptions = {
+		"placeholder": "https:\/\/iar.ac.in\/wp-content\/plugins\/custom-facebook-feed\/assets\/img\/placeholder.png"
+	};
+</script>
+<script src="js/cff-scripts.js" id="cffscripts-js"></script>
+<script src="js/new-tab.js" id="page-links-to-js"></script>
+<script defer src="js/forms.js" id="mc4wp-forms-api-js"></script>
+<script>
+	/(trident|msie)/i.test(navigator.userAgent) && document.getElementById && window.addEventListener && window.addEventListener("hashchange", function () {
+		var t, e = location.hash.substring(1);
+		/^[A-z0-9_-]+$/.test(e) && (t = document.getElementById(e)) && (/^(?:a|select|input|button|textarea)$/i.test(t.tagName) || (t.tabIndex = -1), t.focus())
+	}, !1);
+</script>
+<script type="text/javascript">
+	function tickerSlick() {
+		setTimeout(function () {
+			var slideCount = null;
+			$ticker = jQuery("#ticker01");
+			if ($ticker.hasClass("slick-initialized")) {
+				$ticker.slick("unslick");
+			}
+			$ticker.on("init", function (event, slick) {
+				slideCount = slick.slideCount;
+				jQuery("span.tst").text(slideCount);
+			});
+			$ticker.slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				vertical: true,
+				infinite: true,
+				autoplay: true,
+				speed: 300,
+				prevArrow: ".ticker-prev",
+				nextArrow: ".ticker-next",
+			});
+			$ticker.on(
+				"beforeChange",
+				function (event, slick, currentSlide, nextSlide) {
+					setCurrentSlideNumber(nextSlide);
+				}
+			);
+
+			function setCurrentSlideNumber(currentSlide) {
+				jQuery("span.csc").text(currentSlide + 1);
+			}
+		}, 1);
+	}
+	tickerSlick();
+</script>
+</body>
+
+</html>
---
+++ includes/slider.php
@@ -0,0 +1,88 @@
+<section class="slider-banner-iar">
+    <div class="slick_slider">
+        <div class="single-slide-wrap slide-bg-1">
+            <div class="container-fluid p-0">
+
+                <div class="banner_slider_img desktop_view">
+                    <img src="images/uetr-banner.jpg" class="img-fluid" />
+                </div>
+
+                <div class="banner_slider_img mobile_view">
+                    <img src="images/uetr-banner.jpg" alt="1900X700" class="img-fluid" />
+                </div>
+
+                <div class="row">
+
+                    <div class="col-lg-5 col-md-6 col-sm-9 float_slider_banner">
+                        <div class="slider-content">
+                            <div class="span-title">
+                                <span>ADMISSION OPEN 2022</span>
+                            </div>
+
+                            <div class="main-title-iar">
+                                <h2>Start Your Journey With IAR 4</h2>
+                                <p>The Institute Of Advanced Research (IAR) Is an Innovation modern university, which
+                                    offers professionally focused undergraduate, postgraduate and research degree
+                                    programs.</p>
+                            </div>
+
+                            <div class="btn-iar-banner slider-btn">
+                                <a href="#"> Read More </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="single-slide-wrap slide-bg-1">
+            <div class="container-fluid p-0">
+
+                <div class="banner_slider_img desktop_view">
+                    <img src="images/Carousal1.jpg" class="img-fluid" />
+                </div>
+
+                <div class="banner_slider_img mobile_view">
+                    <img src="images/Carousal1.jpg" alt="1900X700" class="img-fluid" />
+                </div>
+
+                <div class="row">
+
+                    <div class="col-lg-5 col-md-6 col-sm-9 float_slider_banner">
+                        <div class="slider-content">
+                            <div class="span-title">
+                                <span></span>
+                            </div>
+
+                            <div class="main-title-iar">
+                                <h2>Outstanding IAR Student Start-Up</h2>
+                                <p></p>
+                            </div>
+
+                            <div class="btn-iar-banner slider-btn">
+                                <a href="#"> Read More </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="slick-controls">
+
+        <button class="previous control-next btn slick-arrow">
+            <span><i class="fas fa-angle-left"></i></span>
+        </button>
+
+        <button class="next control-prev btn slick-arrow">
+            <span><i class="fas fa-angle-right"></i></span>
+        </button>
+
+    </div>
+
+</section>
