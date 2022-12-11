222cf3a4f7fc08d1d3f10e6762f367ed0da33dbe
Harendra Chauhan <charendra71@gmail.com>
2022-12-11 13:35:47 +0530

    -

--- pages/about-us/the-university.php
+++ pages/about-us/the-university.php
@@ -1,9 +1,33 @@
 <?php
-include('../../includes/header.php');
+include("../../includes/header.php");
 ?>
 
+<section class="rs-breadcrumbs"
+    style="background-image: url(https://iar.ac.in/wp-content/uploads/2022/03/the-trust.jpg);">
+    <div class="breadcrumbs-text">
+        <h1 class="page-title">The University</h1>
+        <nav aria-label="breadcrumb">
+            <ul class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                <li class="breadcrumb-item " aria-current="page"><a href="/about-us/">About Us</a></li>
+                <li class="breadcrumb-item active" aria-current="page"><span>The University</span></li>
+            </ul>
+        </nav>
+    </div>
+</section>
 
+<section class="custom-banner-mtb">
+    <div class="row">
+        <div class="col-md-6">
+            <img src="https://iar.ac.in/wp-content/uploads/2022/03/1-About-IAR.jpg" class="img-fluid" alt="" />
+        </div>
+        <div class="col-md-6">
+                <h1>About Us</h1>
+                <p>The COER university of engineering, which offers professionally focused undergraduate and postgraduate programs.</p>
+        </div>
+    </div>
+</section>
 
 <?php
-include('../../includes/footer.php');
+include("../../includes/footer.php");
 ?>
